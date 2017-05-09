efe3eed82daecc9f644dac920614439203f17a40
Dainty Jen Matundan <dhaintyjhen.matundan@gmail.com>
2017-05-09 17:40:03 +0800

    Add Links (#18)
    * pangalawa (#3)
    
    * Added Footer
    
    * testing
    
    * Super Modified (#5)
    
    * testing ulet  (#6)
    
    * Added Footer
    
    * testing
    
    * for testing
    
    * Try  (#9)
    
    * pangalawa (#3) (#4)
    
    * Added Footer
    
    * testing
    
    * Delete Last Changes
    
    * Try (#8)
    
    * pangalawa (#3)
    
    * Added Footer
    
    * testing
    
    * Super Modified (#5)
    
    * testing ulet  (#6)
    
    * Added Footer
    
    * testing
    
    * for testing
    
    * Delete Changes
    
    * Testing
    
    * Testing
    
    * Add CSS Links (#11)
    
    * pangalawa (#3) (#4)
    
    * Added Footer
    
    * testing
    
    * Delete Last Changes
    
    * Try (#8)
    
    * pangalawa (#3)
    
    * Added Footer
    
    * testing
    
    * Super Modified (#5)
    
    * testing ulet  (#6)
    
    * Added Footer
    
    * testing
    
    * for testing
    
    * Delete Changes
    
    * Testing
    
    * Testing
    
    * Add CSS links
    
    * Add scripts (#15)
    
    * pangalawa (#3) (#4)
    
    * Added Footer
    
    * testing
    
    * Delete Last Changes
    
    * Try (#8)
    
    * pangalawa (#3)
    
    * Added Footer
    
    * testing
    
    * Super Modified (#5)
    
    * testing ulet  (#6)
    
    * Added Footer
    
    * testing
    
    * for testing
    
    * Delete Changes
    
    * Testing
    
    * Testing
    
    * Add CSS links
    
    * Add Scripts
    
    * add links (#17)
    
    * first commit
    
    * first commit
    
    * first commit
    
    * first commit
    
    * origin
    
    * first commit
    
    * first commit
    
    * origin
    
    * asdad
    
    * Add Links

--- js/app.js
+++ js/app.js
@@ -13,18 +13,15 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
       $scope.relatedBooks = res.data.items;
       $scope.bookInfo = res.data.items[0].volumeInfo;
       $scope.saleInfo = res.data.items[0].saleInfo;
+      $scope.related = response.data;
     });
-       $http.get("https://www.googleapis.com/books/v1/volumes?q=" + $scope.search)
-        .then(function(response) {
-          $scope.related = response.data;
-     });
+
   }
 
   $scope.update = function(book) {
       $scope.search = book.volumeInfo.title;
     };
 
- $scope.results = "Nothing to search..."
 
  //Header Image
   $scope.imagePath = 'http://res.cloudinary.com/dvirpbo6t/image/upload/v1493261790/Untitled_au0soa.png';
