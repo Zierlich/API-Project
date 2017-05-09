2006d764034eb695bcc0e112ae62bd1a2cb225e3
cathlynjoy <cathlynjoy.diomampo@gmail.com>
2017-05-09 01:50:19 -0700

    Removed hover function

--- js/app.js
+++ js/app.js
@@ -30,12 +30,6 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
   $scope.imagePath = 'http://res.cloudinary.com/dvirpbo6t/image/upload/v1493261790/Untitled_au0soa.png';
 });
 
-$('#pic').hover(function() {
-  $(this).attr('src', 'https://3.basecamp.com/3507221/blobs/c171e7396933597193d9ca683b78914b0010/download/http---res.cloudinary.com-dvirpbo6t-image-upload-v1493085668-dainty_u3j11z.jpg');
-}, function() {
-  $(this).attr('src', 'https://cdn3.iconfinder.com/data/icons/internet-and-web-4/78/internt_web_technology-13-512.png');
-});
-
 /**
 Copyright 2016 Google Inc. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
