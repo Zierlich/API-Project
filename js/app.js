2043d155e8b84b5f6df0cc7796100b0e446c90a4
dawen22 <drosales.bos@gmail.com>
2017-05-09 17:26:35 +0800

    Add Links

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-
-.controller("bookCtrl",function($scope,$http) {
-    $scope.$watch('search', function() {
-    fetch();
-    });
-
-    $scope.search = "harry potter";
-
-    function fetch() {
-=======
 
 angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
@@ -20,64 +8,20 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
         $scope.search = "";
 
         function fetch() {
->>>>>>> 2bd530b6a1da688d55f6c1f67300d8e0bd8f6349
     $http.get("https://www.googleapis.com/books/v1/volumes?q=" + $scope.search).then(function(res) {
   		console.log(res.data);
       $scope.relatedBooks = res.data.items;
       $scope.bookInfo = res.data.items[0].volumeInfo;
       $scope.saleInfo = res.data.items[0].saleInfo;
-<<<<<<< HEAD
-      $scope.related = res.data;
+      $scope.related = response.data;
     });
 
-
-  }
-    $scope.update = function(book) {
-      $scope.search = book.volumeInfo.title;
-    };
-	})
-
-
-//AppCtrl
-.controller('AppCtrl', function($scope, $mdDialog) {
-  $scope.status = '  ';
-  $scope.customFullscreen = false;
-  $scope.showTabDialog = function(ev) {
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: 'tabDialog.tmpl.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true
-    })
-  };
-  function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
-      $mdDialog.hide();
-    };
-
-    $scope.cancel = function() {
-      $mdDialog.cancel();
-    };
-
-    $scope.answer = function(answer) {
-      $mdDialog.hide(answer);
-    };
-  }
-});
-=======
-    });
-       $http.get("https://www.googleapis.com/books/v1/volumes?q=" + $scope.search)
-        .then(function(response) {
-          $scope.related = response.data;
-     });
   }
 
   $scope.update = function(book) {
       $scope.search = book.volumeInfo.title;
     };
 
- $scope.results = "Nothing to search..."
 
  //Header Image
   $scope.imagePath = 'http://res.cloudinary.com/dvirpbo6t/image/upload/v1493261790/Untitled_au0soa.png';
@@ -93,4 +37,3 @@ $('#pic').hover(function() {
 Copyright 2016 Google Inc. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
 **/
->>>>>>> 2bd530b6a1da688d55f6c1f67300d8e0bd8f6349
